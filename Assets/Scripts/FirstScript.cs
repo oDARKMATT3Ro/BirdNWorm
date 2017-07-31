b4d58c7c5765f85e78c91e0869b3629cc0680ff6
Geoff GoeresHill <geoff.goeres@gmail.com>
2017-07-30 22:33:52 -0700

    Added movement to main character

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstScript : MonoBehaviour {
+public class FirstScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+        Debug.Log("Hello from the Start Method!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(.1f, 0f, 0f);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(-.1f, 0f, 0f);
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(0f, .1f, 0f);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(0f, -.1f, 0f);
+        }
+    }
 }
